513dec5acc573782f50ebc3bafb9f9afa5797cd6
Giordon Stark <kratsg@gmail.com>
2017-02-28 20:58:49 -0600

    add dist/ to prepare for future webpack

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.use(function(req, res, next){
     res.locals.env = process.env;
