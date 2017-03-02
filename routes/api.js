5b6797c85f8776867d37c9744ab763e11823b649
Giordon Stark <kratsg@gmail.com>
2017-03-01 22:26:42 -0600

    add configurability for caching

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var apicache = require('apicache');
 
-var cache = require('apicache').middleware;
+var cache = apicache.middleware;
+if(process.env.REDIS_CACHE === '1'){
+  cache = require('apicache')
+           .options({ redisClient: require('redis').createClient() })
+           .middleware;
+}
 
 router.get('/', cache('5 seconds'), function(req, res, next) {
   res.json({ message: 'API is functioning.', time: + new Date() });
