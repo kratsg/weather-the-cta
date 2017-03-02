ea5fce5c2a351216e479a9c584afe90c43f101d4
Giordon Stark <kratsg@gmail.com>
2017-03-01 22:03:24 -0600

    add routes and cache

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.json({ message: 'respond with a resource', time: + new Date() });
+var cache = require('apicache').middleware;
+
+router.get('/', cache('5 seconds'), function(req, res, next) {
+  res.json({ message: 'API is functioning.', time: + new Date() });
+});
+
+router.get('/buses', cache('1 minute'), function(req, res, next) {
+  res.json({ message: 'Return a list of buses and times', time: + new Date() });
+});
+
+router.get('/weather', cache('5 minutes'), function(req, res, next) {
+  res.json({ message: 'Return current weather', time: + new Date() });
 });
 
 module.exports = router;
