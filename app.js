e9eac858e68e317cc0c0f5a606289844bc82147f
Giordon Stark <kratsg@gmail.com>
2017-03-01 21:59:20 -0600

    users->api

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -33,7 +33,7 @@ app.use(function(req, res, next){
 });
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
