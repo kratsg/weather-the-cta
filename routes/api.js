a37aa3a5199186082cf6411eaa02bc5a35833f0b
Giordon Stark <kratsg@gmail.com>
2017-03-02 00:18:30 -0600

    return the weather

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,8 @@ if(process.env.REDIS_CACHE === '1'){
            .middleware;
 }
 
+var request = require('request');
+
 router.get('/', cache('5 seconds'), function(req, res, next) {
   res.json({ message: 'API is functioning.', time: + new Date() });
 });
@@ -17,8 +19,19 @@ router.get('/buses', cache('1 minute'), function(req, res, next) {
   res.json({ message: 'Return a list of buses and times', time: + new Date() });
 });
 
-router.get('/weather', cache('10 minutes'), function(req, res, next) {
-  res.json({ message: 'Return current weather', time: + new Date() });
+router.get('/weather', cache('5 minutes'), function(req, res, next) {
+  request(
+    {
+      uri: 'https://api.darksky.net/forecast/'+req.darksky_key+'/'+req.lat_long,
+      json: true,
+      gzip: true
+    }, function(error, response, body){
+        console.log(response.request.uri);
+        console.log('server encoded the data as: ' + (response.headers['content-encoding'] || 'identity'))
+        if (error && response.statusCode != 200) return next(error);
+        res.json(body);
+    }
+  );
 });
 
 module.exports = router;
