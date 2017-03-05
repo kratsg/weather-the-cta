cac1c4acd3df28e38b73cfbc0dfb4baf272d2ecc
Giordon Stark <kratsg@gmail.com>
2017-03-04 23:44:59 -0600

    Giordon's first timer.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// use createClass to get autobinding working, doesn't work in ES6
+//      http://blog.andrewray.me/react-es6-autobinding-and-createclass/
+var WeatherWidget = React.createClass({
+  getInitialState: function() {
+    return {seconds: 0};
+  },
+  componentWillMount: function() {
+    this.interval = null;
+  },
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, this.props.delta);
+  },
+  tick: function() {
+    this.setState({seconds: this.state.seconds + 1});
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+  render: function() {
+    return <h1>Hello, {this.props.name}. I have been running for {this.state.seconds} seconds.</h1>
+  }
+});
+
+WeatherWidget.defaultProps = {name: 'Giordon', delta: 1000};
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <WeatherWidget />,
   document.getElementById('root')
 );
