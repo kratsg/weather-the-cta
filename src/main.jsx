2e653e358d9ed0c73938305744ce2fa57dc25cb4
Giordon Stark <kratsg@gmail.com>
2017-03-05 02:24:16 -0600

    HOCs are better for abstraction

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// use createClass to get autobinding working, doesn't work in ES6
+// Use arrow-function-in-constructor to bind, cannot bind class methods
 //      http://blog.andrewray.me/react-es6-autobinding-and-createclass/
-var WeatherWidget = React.createClass({
-  getInitialState: function() {
-    return {seconds: 0};
-  },
-  componentWillMount: function() {
-    this.interval = null;
-  },
-  componentDidMount: function() {
-    this.interval = setInterval(this.tick, this.props.delta);
-  },
-  tick: function() {
-    this.setState({seconds: this.state.seconds + 1});
-  },
-  componentWillUnmount: function() {
-    clearInterval(this.interval);
-  },
-  render: function() {
-    return <h1>Hello, {this.props.name}. I have been running for {this.state.seconds} seconds.</h1>
-  }
-});
+function withSubscription(WrappedComponent, fetchData) {
+  class SubscribedComponent extends React.Component {
+    constructor(props) {
+      super(props);
 
-WeatherWidget.defaultProps = {name: 'Giordon', delta: 1000};
+      this.interval = null;
+      this.state = {timedelta: this.props.timedelta, counter: 0, data: fetchData(props)};
+      this.tick = () => this.setState((prevState, props) => ({counter: prevState.counter + 1, data: fetchData(props)}));
+    }
+
+    startUpdates() {
+      this.interval = setInterval(this.tick, this.state.timedelta);
+    }
+
+    stopUpdates() {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+
+    componentWillMount() {
+      this.interval = null;
+    }
+
+    componentDidMount() {
+      this.startUpdates();
+    }
+
+    componentWillUnmount() {
+      this.stopUpdates();
+    }
+
+    componentWillReceiveProps(nextProps) {
+      console.log('receive props');
+      if(nextProps.timedelta !== this.state.timedelta){
+        console.log('it changed!');
+        this.stopUpdates();
+        this.setState({ timedelta: nextProps.timedelta });
+        this.startUpdates();
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.state} {...this.props}/>
+    }
+  }
+  SubscribedComponent.defaultProps = {name: 'Giordon', timedelta: 1000};
+  return SubscribedComponent;
+}
+
+class WeatherWidget extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}. I have run the subscription {this.props.counter} times.</h1>
+  }
+}
+
+const SubscribedWeatherWidget = withSubscription(WeatherWidget, () => {});
 
 ReactDOM.render(
-  <WeatherWidget />,
+  <SubscribedWeatherWidget timedelta='5000' />,
   document.getElementById('root')
 );
