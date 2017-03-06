8ab42542d54784b2d065b4686d54b8f8c4132508
Giordon Stark <kratsg@gmail.com>
2017-03-05 21:38:56 -0600

    use promises in the requests

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@ if(process.env.REDIS_CACHE === '1'){
            .middleware;
 }
 
-var request = require('request');
+var request = require('request-promise');
 
 router.get('/', cache('5 seconds'), function(req, res, next) {
   res.json({ message: 'API is functioning.', time: + new Date() });
@@ -20,18 +20,16 @@ router.get('/buses', cache('1 minute'), function(req, res, next) {
 });
 
 router.get('/weather', cache('5 minutes'), function(req, res, next) {
-  request(
-    {
+  request({
       uri: 'https://api.darksky.net/forecast/'+req.darksky_key+'/'+req.lat_long,
       json: true,
-      gzip: true
-    }, function(error, response, body){
-        console.log(response.request.uri);
-        console.log('server encoded the data as: ' + (response.headers['content-encoding'] || 'identity'))
-        if (error && response.statusCode != 200) return next(error);
-        res.json(body);
-    }
-  );
+      gzip: true,
+      resolveWithFullResponse: true
+  }).then(function(response){
+      console.log(response.request.uri);
+      console.log('server encoded the data as: ' + (response.headers['content-encoding'] || 'identity'))
+      res.json(response.body);
+  }).catch(next);
 });
 
 module.exports = router;
