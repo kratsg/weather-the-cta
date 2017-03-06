e7c8e17255b8e1def76549d70dff246fe12b7004
Giordon Stark <kratsg@gmail.com>
2017-03-05 23:35:58 -0600

    dump the request object that we get from the server

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import request from 'request-promise';
 
 import { Logged } from './logging';
 import { Subscribed } from './subscription';
 
+var renderRecursive = (path, index, val) => {
+  if(val instanceof Object)
+    return <Dict key={path} path={path} index={index} data={val} />
+  else if(val instanceof Array)
+    return <List key={path} path={path} index={index} data={val} />
+  else
+    return <span>{val}</span>
+}
+
+class Item extends React.Component {
+  render() {
+    return <li>
+      {this.props.index}:
+      { renderRecursive(this.props.path, this.props.index, this.props.data) }
+    </li>
+  }
+}
+
+class List extends React.Component {
+  dump(data) {
+    return data.map((key, index) => <Item key={this.props.path.replace(/\/$/,'') + '/' + key} path={this.props.path.replace(/\/$/,'') + '/' + key} index={index} data={key} />)
+  }
+
+  render() {
+    return <ol>
+      {this.dump(this.props.data)}
+    </ol>
+  }
+}
+
+class Dict extends React.Component {
+    dump(data) {
+    return Object.keys(data).map((key, index) => <Item key={this.props.path.replace(/\/$/,'') + '/' + key} path={this.props.path.replace(/\/$/,'') + '/' + key} index={key} data={data[key]} />)
+  }
+
+  render() {
+    return <ul>
+      { this.dump(this.props.data) }
+    </ul>
+  }
+}
+Dict.defaultProps = {index: '/', path: '/'}
+
 class WeatherWidget extends React.Component {
   render() {
-    if(!Object.keys(this.props.data).length) return (<div><h1>No data yet...</h1></div>);
+    if(this.props.data === undefined || !Object.keys(this.props.data).length) return (<div><h1>No data yet...</h1></div>);
     return (
       <div>
         <h1>Data</h1>
-        <ul>
-        { Object.keys(this.props.data).map((key) => <li key={key}>{key}: {this.props.data[key]}</li>) }
-        </ul>
+        <Dict data={this.props.data} />
       </div>
     );
   }
 }
 
-const SubscribedWeatherWidget = Logged(Subscribed(WeatherWidget, () => Promise.resolve({name: 'Giordon'})));
+const SubscribedWeatherWidget = Logged(Subscribed(WeatherWidget, () => request({uri: `${document.location.href.replace(/\/$/,'')}/api/weather`, json: true}).catch(console.error)));
 
 ReactDOM.render(
   <SubscribedWeatherWidget timedelta='5000' />,
