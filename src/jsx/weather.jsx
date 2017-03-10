55531426e6f8d75b3bfd7be287d25020f5d8f5fd
Giordon Stark <kratsg@gmail.com>
2017-03-10 13:29:59 -0600

    current conditions and forecast added

--- src/jsx/weather.jsx
+++ src/jsx/weather.jsx
@@ -14,6 +14,7 @@ var formatDate = timestamp => {
   var month = d.getMonth()
   var year = d.getFullYear()
   var hour = d.getHours()
+  hour = ('00'+hour).substring((hour+"").length)
   var min = d.getMinutes()
   min = ('00'+min).substring((min+"").length)
 
@@ -22,7 +23,7 @@ var formatDate = timestamp => {
   else if (date%10 == 3) superscript = "rd";
   else                   superscript = "th"
 
-  return days[day]+' '+months[month]+' '+date+superscript+' '+year+' @'+hour+min+'h';
+  return [days[day]+' '+months[month]+' '+date+superscript+' '+year, hour+':'+min];
 };
 
 export class WeatherWidget extends React.Component {
@@ -33,7 +34,7 @@ export class WeatherWidget extends React.Component {
         <div className="card-header" style={cardHeaderStyle}>
           <h1 className="display-1">
             <i className="fa fa-cog fa-spin fa-fw" aria-hidden="true"></i><span className="sr-only">Loading...</span>
-            Loading weather data...
+           &nbsp; Loading weather data...
           </h1>
         </div>
         <div className="card-block">
@@ -48,21 +49,54 @@ export class WeatherWidget extends React.Component {
       </div>
     );
     return (
-    <div className="card card-inverse bg-inverse">
-      <div className="card-header" style={cardHeaderStyle}>
-        <h1 className="display-1">
-          <i className={`wi wi-forecast-io-${this.props.data.currently.icon} fa-fw`} aria-hidden="true" /><span className="sr-only">Current weather conditions</span>
-          {this.props.data.currently.temperature}&deg;F
-        </h1>
+    <div className="card-deck">
+      <div className="card card-inverse bg-inverse">
+        <div className="card-header" style={cardHeaderStyle}>
+          <h1 className="display-1">
+            Current Conditions
+          </h1>
+        </div>
+        <div className="card-block">
+          <div className="row">
+            <div className="col-2">
+              <i className={`fa-5x wi wi-forecast-io-${this.props.data.currently.icon} fa-fw`} aria-hidden="true" /><span className="sr-only">Current weather conditions</span>
+            </div>
+            <div className="col-10">
+              <h1 className="card-title">{Math.round(this.props.data.currently.temperature)}<i className="wi wi-degrees" />, feels like {Math.round(this.props.data.currently.apparentTemperature)}<i className="wi wi-degrees" /></h1>
+              <h3 className="card-text">{this.props.data.currently.summary}</h3>
+            </div>
+          </div>
+        </div>
+        <div className="card-footer text-muted">
+          <div className="row">
+            <p className="col-sm text-left">{formatDate(this.props.data.currently.time*1000)[1]}</p>
+            <p className="col-sm text-sm-right">{this.props.data.latitude}, {this.props.data.longitude}</p>
+          </div>
+        </div>
       </div>
-      <div className="card-block">
-        <h1 className="card-title">Summary</h1>
-        <h3 className="card-text">{this.props.data.currently.summary}</h3>
-      </div>
-      <div className="card-footer text-muted">
-        <div className="row">
-          <p className="col-sm text-left">{formatDate(this.props.data.currently.time*1000)}</p>
-          <p className="col-sm text-sm-right">{this.props.data.latitude}, {this.props.data.longitude}</p>
+      <div className="card card-inverse bg-inverse">
+        <div className="card-header" style={cardHeaderStyle}>
+          <h1 className="display-1">
+            Today's Forecast
+          </h1>
+        </div>
+        <div className="card-block">
+          <div className="row">
+            <div className="col-2">
+              <i className={`fa-5x wi wi-forecast-io-${this.props.data.daily.icon} fa-fw`} aria-hidden="true" /><span className="sr-only">Today's weather forecast</span>
+            </div>
+            <div className="col-10">
+              <h1 className="card-title">
+                <span className="text-primary">{Math.round(this.props.data.daily.data[0].temperatureMax)}<i className="wi wi-degrees" /></span> <span className="text-info">{Math.round(this.props.data.daily.data[0].temperatureMin)}<i className="wi wi-degrees" /></span></h1>
+              <h3 className="card-text">{this.props.data.daily.summary}</h3>
+            </div>
+          </div>
+        </div>
+        <div className="card-footer text-muted">
+          <div className="row">
+            <p className="col-sm text-left">{formatDate(this.props.data.daily.data[0].time*1000)[0]}</p>
+            <p className="col-sm text-sm-right">{this.props.data.latitude}, {this.props.data.longitude}</p>
+          </div>
         </div>
       </div>
     </div>);
