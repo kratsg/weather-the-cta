2fd7b1a8bbb2dc0a49e575d860fcc4fb903a6691
Giordon Stark <kratsg@gmail.com>
2017-03-09 19:39:40 -0600

    add sass compilation

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,25 @@ module.exports = function(grunt) {
           'dist/css/bootstrap.css': 'src/sass/bootstrap.scss'
         }
       }
+    },
+    cssmin: {
+      minify: {
+        files: [{
+          expand: true,
+          cwd: 'dist/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/css',
+          ext: '.min.css',
+        }]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-webpack');
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['webpack:main', 'sass']);
+  grunt.registerTask('default', ['webpack', 'sass','cssmin']);
 
 };
