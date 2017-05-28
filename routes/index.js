8a2218de5ba4f7a90789d676828b9a9fd7a7f6c4
Giordon Stark <kratsg@gmail.com>
2017-05-28 12:34:36 -0500

    don't send favicon. https://stackoverflow.com/a/35408810/1532974

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/favicon.ico', function(req, res, next) {
+    res.sendStatus(204);
+});
+
 module.exports = router;
