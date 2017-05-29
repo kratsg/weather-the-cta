90673ace852ee50a81bceeb5472057261df3b701
Giordon Stark <kratsg@gmail.com>
2017-05-28 23:19:34 -0500

    set up structure for CTA Bus data

--- src/jsx/transit.jsx
+++ src/jsx/transit.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
 
-export class TransitWidget extends React.Component {
+export class CTABusesWidget extends React.Component {
   render() {
-    var cardHeaderStyle = {backgroundColor: '#222'}
-    if(!this.data) return;
+    if(!this.props.data) return(<div />);
     return (
     <div className="card-deck">
-      <div className="card card-inverse bg-inverse">
-        <div className="card-header" style={cardHeaderStyle}>
-          <h1 className="display-2">
-            Bus Times
-          </h1>
-        </div>
-        <div className="card-block">
-          <div className="row">
-            <div className="col-2">
-              Vroom
-            </div>
-            <div className="col-10">
-              <h1 className="card-title display-3">Bus?</h1>
-              <h3 className="card-text display-4">Coming in some time...</h3>
-            </div>
-          </div>
-        </div>
-      </div>
+      {this.props.data.prd.map( busPrediction => <CTABusPrediction key={busPrediction.stpid} data={busPrediction} /> )}
     </div>);
+  }
+}
+
+class CTABusPrediction extends React.Component {
+  render() {
+    return(
+    <div className="card card-inverse bg-inverse">
+      {this.props.data.rtdir} #{this.props.data.rt} {this.props.data.typ == 'A' ? ' arriving at ' : ' departing from '} {this.props.data.stpnm} in {this.props.data.prdctdn} minutes.
+    </div>
+    );
   }
 }
